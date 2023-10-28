792a83a36d221e9e4bb1ff2f025c61c0ab5a32dd
andy henrie <ergonyc@gmail.com>
2023-10-27 17:50:48 -0700

    first update

---
+++ README.md
@@ -0,0 +1,213 @@
+<a name="readme-top"></a>
+
+<!-- PROJECT SHIELDS -->
+<!--
+*** I'm using markdown "reference style" links for readability.
+*** Reference links are enclosed in brackets [ ] instead of parentheses ( ).
+*** See the bottom of this document for the declaration of the reference variables
+*** for contributors-url, forks-url, etc. This is an optional, concise syntax you may use.
+*** https://www.markdownguide.org/basic-syntax/#reference-style-links
+-->
+
+<!-- PROJECT LOGO -->
+<br />
+<div align="center">
+  <a href="https://github.com/AMCalejandro/gp2-manifest-checkupload/tree/main/">
+    <img src="apps/img/GP2_logo_color.png" alt="Logo" width="600" height="200">
+  </a>
+
+  <h3 align="center">GP2 CIWG data QC - Upload - Visualise app</h3>
+
+  <p align="center">
+    An app that allows you to QC your data, then upload it to GP2 system. You will also be able to visualise your data
+    <br />
+    <br />
+  </p>
+</div>
+
+
+
+<!-- TABLE OF CONTENTS -->
+<details>
+  <summary>Table of Contents</summary>
+  <ol>
+    <li>
+      <a href="#about-the-app">About the app</a>
+      <ul>
+        <li><a href="#built-with">Built With</a></li>
+      </ul>
+    </li>
+    <li>
+      <a href="#getting-started">Getting Started</a>
+      <ul>
+        <li><a href="#prerequisites">Prerequisites</a></li>
+        <li><a href="#installation">Installation</a></li>
+      </ul>
+    </li>
+    <li><a href="#usage">Usage</a></li>
+    <li><a href="#contact">Contact</a></li>
+    <li><a href="#acknowledgments">Acknowledgments</a></li>
+  </ol>
+</details>
+
+
+<!-- ABOUT THE PROJECT -->
+## About the app
+
+Welcome to the ASAP CRN metadata QC app. This is an application intended to make easier the burden of harmonizing the data to ASAP CRN standards. 
+
+In the future this may also  assign ASAP CRN IDs.
+
+
+The intended workflow is as follow:
+* upload csvs to to the app ( clinical tab)
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+### Built With
+
+Here I summarise the main software we have used to build the app
+
+
+* [![Python][py-shield]][python-url]
+
+* [![Streamlit][st-shield]][st-url]
+
+* [![Docker][do-shield]][do-url]
+
+* [![GCP][gcp-shield]][gcp-url]
+
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+<!-- GETTING STARTED -->
+## Getting Started
+
+Here, I summarise some points to efficiently use the app.
+* To perform clinical data QC and then visualise, please make sure you QC and upload the sample manifest for your samples
+* If your are QC ing several sample manifests at a time, please refresh the app between sample manifests. The  app makes use of something called streamlit session state to propagate data across apps, you need to release the cache between sample manifests first.
+* You can only visualise your data if both the sample manifest and the clinical data have been QCed. Otherwise an error message will pop up
+
+
+### Prerequisites
+
+* Docker installed
+* Python3.9 or higher
+* Access to the GCP project in which the app is hosted
+
+
+### Installation 
+
+If you wanted to run this app locally and play with it, it should be ass simple as:
+
+1. Clone the github repository
+
+```bash
+git clone git@github.com:ergonyc/asap_sc_collect.git 
+```
+
+2. Make sure you have access to some paths hard coded within the app not added on the github remote
+
+3. Build the docker container that contains the app
+
+```bash
+docker build -t <docker-container-name> .
+```
+
+4. Run the docker container on a port
+
+```bash
+docker run -d -p 8080:8080 <docker-container-name>
+```
+
+5. Alternatively, you can also build virtual environment, then run the streamlit app locally
+
+```bash
+streamlit run app.py
+
+```
+
+
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- USAGE EXAMPLES -->
+## Usage
+
+Use the navigation bar at the top of the app to move around the tabs available on the app. 
+
+Once you upload the data to either the sample manifest or the clinical data tabs, you should be able to follow along and understand how the app is intended to help you harmonising your data to GP2 standards.
+
+Same way, you should be able to easily upload your data the storage system and the visualization tabs and follow along.
+
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTRIBUTING -->
+## Contributing
+
+Contributions are what make the open source community such an amazing place to learn, inspire, and create. Any contributions you make are **greatly appreciated**.
+
+If you have a suggestion that would make this better, please fork the repo and create a pull request.
+Thanks again!
+
+1. Fork the Project
+2. Create your Feature Branch (`git checkout -b feature/AmazingFeature`)
+3. Commit your Changes (`git commit -m 'Add some AmazingFeature'`)
+4. Push to the Branch (`git push origin feature/AmazingFeature`)
+5. Open a Pull Request
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- LICENSE -->
+## License
+
+Distributed under the MIT License. See `LICENSE.txt` for more information.
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- CONTACT -->
+## Contact
+
+Also email me on henrie@datatecnica.com
+
+Project Link: [TBD]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- ACKNOWLEDGMENTS -->
+## Acknowledgments
+
+This would not be possible without help of the Data Tecnica team, specially Alejandro Martinez, and the similar GP2 tool 
+* [DtI](https://www.datatecnica.com/)
+* [ASAP CRN]
+
+<p align="right">(<a href="#readme-top">back to top</a>)</p>
+
+
+
+<!-- MARKDOWN LINKS & IMAGES -->
+<!-- https://www.markdownguide.org/basic-syntax/#reference-style-links -->
+[do-shield]: https://img.shields.io/badge/docker-%230db7ed.svg?style=for-the-badge&logo=docker&logoColor=white
+[do-url]: https://www.docker.com/
+[st-shield]: https://user-images.githubusercontent.com/7164864/217935870-c0bc60a3-6fc0-4047-b011-7b4c59488c91.png
+[st-url]: https://streamlit.io/
+[py-shield]: https://img.shields.io/badge/python-3670A0?style=for-the-badge&logo=python&logoColor=ffdd54
+[python-url]: https://www.python.org/
+[gcp-url]: https://cloud.google.com/?hl=en
--- app.py
+++ app.py
@@ -15,36 +15,152 @@ def read_file(data_file):
 
 
 
+def jumptwice():
+    st.write("##")
+    st.write("##")
+
+
+def validate_table(table: pd.DataFrame, table_name: str, CDE: pd.DataFrame):
+
+    retval = 1
+
+    # Filter out rows specific to the given table_name from the CDE
+    specific_cde_df = CDE[CDE['Table'] == table_name]
+    
+    # Extract fields that have a data type of "Enum" and retrieve their validation entries
+    enum_fields_dict = dict(zip(specific_cde_df[specific_cde_df['DataType'] == "Enum"]['Field'], 
+                               specific_cde_df[specific_cde_df['DataType'] == "Enum"]['Validation']))
+    
+    # Extract fields that are marked as "Required"
+    required_fields = specific_cde_df[specific_cde_df['Required'] == "Required"]['Field'].tolist()
+    optional_fields = specific_cde_df[specific_cde_df['Required'] == "Optional"]['Field'].tolist()
+
+    table = force_enum_string(table, table_name, CDE)
+
+    # Check for missing "Required" fields
+    missing_required_fields = [field for field in required_fields if field not in table.columns]
+    
+    if missing_required_fields:
+        st.error(f"\tMissing Required Fields in {table_name}: {', '.join(missing_required_fields)}")
+    else:
+        st.text(f"\tAll required fields are present in {table_name}.")
+        
+    jumptwice()
+    # Check for empty or NaN values
+    for test_field,test_name in zip([required_fields, optional_fields], ["Required", "Optional"]):
+        empty_or_nan_fields = {}
+        for field in test_field:
+            if field in table.columns:
+                invalid_count = table[field].isna().sum()
+                if invalid_count > 0:
+                    empty_or_nan_fields[field] = invalid_count
+                    
+        if empty_or_nan_fields:
+            st.error(f"\t\t{test_name} Fields with Empty or NaN values:")
+            for field, count in empty_or_nan_fields.items():
+                st.text(f"\t\t\t- {field}: {count} rows")
+            retval = 0
+        else:
+            st.text(f"No empty or NaN values found in {test_name} fields.")
+    
+    jumptwice()
+    # Check for invalid Enum field values
+    invalid_field_values = {}
+    for field, validation_str in enum_fields_dict.items():
+        valid_values = eval(validation_str)
+        if field in table.columns:
+            invalid_values = table[~table[field].isin(valid_values)][field].unique()
+            if invalid_values.any():
+                invalid_field_values[field] = invalid_values
+    
+    if invalid_field_values:
+        st.error("\tInvalid Field/Value pairs:")
+        for field, values in invalid_field_values.items():
+            st.text(f"\t\t\t- {field}: {', '.join(map(str, values))}")
+        retval = 0
+    else:
+        st.text(f"\tAll Enum fields have valid values in {table_name}.")
+
+    return retval
+
+######## HELPERS ########
+# Define a function to only capitalize the first letter of a string
+def capitalize_first_letter(s):
+    if not isinstance(s, str) or len(s) == 0:  # Check if the value is a string and non-empty
+        return s
+    return s[0].upper() + s[1:]
+
+def force_enum_string(df, df_name, CDE):
+
+    string_enum_fields = CDE[(CDE["Table"] == df_name) & 
+                                (CDE["DataType"].isin(["Enum", "String"]))]["Field"].tolist()
+    # Convert the specified columns to string data type using astype() without a loop
+    columns_to_convert = {col: 'str' for col in string_enum_fields if col in df.columns}
+    df = df.astype(columns_to_convert)
+
+    # enum_fields = CDE[ (CDE["Table"] == df_name) & 
+    #                             (CDE["DataType"]=="Enum") ]["Field"].tolist()
+    
+    for col in string_enum_fields:
+        if col in df.columns and col not in ["assay", "file_type"]:
+            df[col] = df[col].apply(capitalize_first_letter)
+
+    return df
+
+
+
 # Show template that ppl must fill in 
 # We can split the template in chunks and process each template sepparatedly if we like
 # As this is just the beginning, let's keep it simple and process all data on a single app. Then, we can easily make it nicer
 
 
 
-# Define optional columns
-optional_cols = ["PI_ORCHID", "PI_google_scholar_id", "metadata_version_date", "primary_diagnosis_text",
-                 "preprocessing_references", "DV200" , "pm_PH", "donor_id", "other_reference", "smoking_years",
-                 "path_autopsy_second_dx", "path_autopsy_third_dx", "path_autopsy_fourth_dx" "path_autopsy_fifth_dx",
-                 "path_autopsy_sixth_dx", "path_autopsy_seventh_dx" "path_autopsy_eight_dx"]
+# # Define optional columns
+# optional_cols = ["PI_ORCHID", "PI_google_scholar_id", "metadata_version_date", "primary_diagnosis_text",
+#                  "preprocessing_references", "DV200" , "pm_PH", "donor_id", "other_reference", "smoking_years",
+#                  "path_autopsy_second_dx", "path_autopsy_third_dx", "path_autopsy_fourth_dx" "path_autopsy_fifth_dx",
+#                  "path_autopsy_sixth_dx", "path_autopsy_seventh_dx" "path_autopsy_eight_dx"]
 
 
 # Provide template
 st.markdown('<p class="big-font"> ASAP single cell data fields self-QC </p>', unsafe_allow_html=True)
 st.markdown('<p class="medium-font"> This app is intended to make sure ASAP contributing with single cell data provide standard ASAP required fields </p>', unsafe_allow_html=True)
 st.markdown('<p class="medium-font"> Download the template from the link below. Once you open the link, go to "File"> "Download" > "xlsx" or "csv" format </p>', unsafe_allow_html=True)
-st.markdown('[Access the data dictionary and template](https://docs.google.com/spreadsheets/d/1ePEyitq8Gy7EZ8hZ4KbF4iAew2g3n_RS/edit#gid=263869186)', unsafe_allow_html=True)
+st.markdown('[Access the data dictionary and template](https://docs.google.com/spreadsheets/d/1xjxLftAyD0B8mPuOKUp5cKMKjkcsrp_zr9yuVULBLG8/edit?usp=sharing)', unsafe_allow_html=True)
 
 
 
 # Read file from streamlit and create a copy to do the maps
-data_file = st.sidebar.file_uploader("Upload Your Sample manifest (CSV/XLSX)", type=['xlsx', 'csv'])
-
-if data_file is not None:
-    data = read_file(data_file)
-else:
+data_file = st.sidebar.file_uploader("Upload Your meta-tables (SAMPLE.csv, STUDY.csv, PROTOCOL.csv, CLINPATH.csv, and/or SUBJECT.csv)", type=['xlsx', 'csv'],accept_multiple_files=True)
+if data_file is None: 
     st.stop()
+elif len(data_file)>1:
+    table = [dat_f.name.split('.')[0] for dat_f in data_file]
+    data = [ read_file(dat_f) for dat_f in data_file]
+else:
+    table = data_file.name.split('.')[0] 
+    data = read_file(data_file)
 
-datamaps_copy = data.copy()
+
+datamaps_copy = [dat.copy() for dat in data]
+# Construct the path to CSD.csv
+cde_file_path = "ASAP_CDE.csv"
+CDE_df = pd.read_csv(cde_file_path)
+
+for table_name,dat in zip(table,data):
+
+    st.subheader(f"## {table_name} ({table_name}.csv)")
+    # data_file = "https://docs.google.com/spreadsheets/d/1xjxLftAyD0B8mPuOKUp5cKMKjkcsrp_zr9yuVULBLG8/edit?usp=sharing"
+    # Load the CDE.csv file and the reference table
+
+    retval = validate_table(dat, table_name, CDE_df)
+    if retval == 0:
+        st.error(f"## {table_name} FAILED !!!!!!!!!!")
+        st.text('First 30 entries with missing data in any required fields')
+        st.write(dat.head(30))
+#     st.text('First 30 entries with missing data in any required fields')
+#     st.write(data_non_miss_check[data_non_miss_check.isna().sum(1)>0].head(30))
+
 
 
 # Check all columns are present in the input 
@@ -53,18 +169,18 @@ datamaps_copy = data.copy()
 
 
 
-# Check all required columns are not missing
-required_cols = [col for col in data.columns if col not in optional_cols]
+# # Check all required columns are not missing
+# required_cols = [col for col in data.columns if col not in optional_cols]
 
-data_non_miss_check = data[required_cols].copy()
+# data_non_miss_check = data[required_cols].copy()
 
-if data_non_miss_check.isna().sum().sum()>0:
-    st.error('There are some missing entries in the required columns. Please fill the missing cells ')
-    st.text('First 30 entries with missing data in any required fields')
-    st.write(data_non_miss_check[data_non_miss_check.isna().sum(1)>0].head(30))
-    st.stop()
-else:
-    st.text('Check missing data in the required fields --> OK')
+# if data_non_miss_check.isna().sum().sum()>0:
+#     st.error('There are some missing entries in the required columns. Please fill the missing cells ')
+#     st.text('First 30 entries with missing data in any required fields')
+#     st.write(data_non_miss_check[data_non_miss_check.isna().sum(1)>0].head(30))
+#     st.stop()
+# else:
+#     st.text('Check missing data in the required fields --> OK')
 
 
 
@@ -72,34 +188,34 @@ else:
 
 
 
-# Example on how to map users code to our standard codes
-# If we use this approach I would like to avoid code repetition and try to wrap this on a function and a for loop 
-# I do not want to have this same thing 100 times
-# Also, let's think if we can come up with something cooler to do this, something that looks nicer
+# # Example on how to map users code to our standard codes
+# # If we use this approach I would like to avoid code repetition and try to wrap this on a function and a for loop 
+# # I do not want to have this same thing 100 times
+# # Also, let's think if we can come up with something cooler to do this, something that looks nicer
 
-# sex for qc
-st.subheader('Create "biological_sex_for_qc"')
-st.text('Count per sex group')
-st.write(data.sex.value_counts())
+# # sex for qc
+# st.subheader('Create "biological_sex_for_qc"')
+# st.text('Count per sex group')
+# st.write(data.sex.value_counts())
 
-sexes=data.sex.dropna().unique()
-n_sexes = st.columns(len(sexes))
-mapdic={}
-for i, x in enumerate(n_sexes):
-    with x:
-        sex = sexes[i]
-        mapdic[sex]=x.selectbox(f"[{sex}]: For QC, please pick a word below",
-                            ["Male", "Female","Intersex","Unnown"], key=i)
-data['sex_qc'] = data.sex.replace(mapdic)
+# sexes=data.sex.dropna().unique()
+# n_sexes = st.columns(len(sexes))
+# mapdic={}
+# for i, x in enumerate(n_sexes):
+#     with x:
+#         sex = sexes[i]
+#         mapdic[sex]=x.selectbox(f"[{sex}]: For QC, please pick a word below",
+#                             ["Male", "Female","Intersex","Unnown"], key=i)
+# data['sex_qc'] = data.sex.replace(mapdic)
 
-# cross-tabulation
-st.text('=== sex_qc x sex ===')
-xtab = data.pivot_table(index='sex_qc', columns='sex', margins=True,
-                        values='sample_id', aggfunc='count', fill_value=0)
-st.write(xtab)
+# # cross-tabulation
+# st.text('=== sex_qc x sex ===')
+# xtab = data.pivot_table(index='sex_qc', columns='sex', margins=True,
+#                         values='sample_id', aggfunc='count', fill_value=0)
+# st.write(xtab)
 
-sex_conf = st.checkbox('Confirm sex_qc?')
-if sex_conf:
-    st.info('Thank you')
+# sex_conf = st.checkbox('Confirm sex_qc?')
+# if sex_conf:
+#     st.info('Thank you')
 
 
